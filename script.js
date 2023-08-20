fc7f7c420745faf9dc2fb1710154abdcb4961b76
Ujjwal Singh <132872642+livujjwal@users.noreply.github.com>
2023-08-20 07:30:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+const selectElement = document.getElementById("colorSelect");
+const btnElement = document.getElementsByTagName("input")[0];
+const optionArray = document.getElementsByTagName("option");
+btnElement.addEventListner("click", () => {
+	const removeItem = optionArray.value;
+	console.log(optionArray);
+	console.log(removeItem);
+});
